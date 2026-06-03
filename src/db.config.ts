997623bb54b202e6392b665792b3b1f6d9cd50e2
BOOK-B9L8E7JSGD\kimsy <kimsy040101@cau.ac.kr>
2026-06-03 13:05:45 +0900

    feat: set dotenv path explicitly in db.config

--- src/db.config.ts
+++ src/db.config.ts
@@ -1,9 +1,13 @@
-
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const adapter = new PrismaMariaDb({
     host: process.env.DB_HOST,
